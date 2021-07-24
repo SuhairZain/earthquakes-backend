61b2040e984024b460d279822fdbcf2edee0c433
Suhair Zain <suhairzain11@gmail.com>
2021-07-25 01:29:11 +0530

    Use CORS library

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,14 @@
-import { getEarthQuakes } from "./controllers/earthQuakes/earthQuakesController";
+import express from "express";
+import cors from "cors";
+
 import "./database/models";
 
-const express = require("express");
+import { getEarthQuakes } from "./controllers/earthQuakes/earthQuakesController";
 
 const app = express();
 
 // Allow CORS
-app.all("/*", function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "X-Requested-With");
-  next();
-});
+app.use(cors({ exposedHeaders: ["X-Total-Count"] }));
 
 app.get("/earthQuakes", getEarthQuakes);
 
